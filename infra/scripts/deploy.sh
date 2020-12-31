6f686e9327810387a5bc32560b4e5282c99abfb3
Amit Kumar <amit.kumar@bluestacks.com>
2020-12-31 19:38:02 +0530

    changed max surge to 25% and 1

--- infra/scripts/deploy.sh
+++ infra/scripts/deploy.sh
@@ -3,5 +3,5 @@ set -o pipefail
 # takes argument as build no to update the app.yaml via sed
 DEPLOYMENT_PATH="infra/k8/app.yaml"
 
-template=`cat ${DEPLOYMENT_PATH} | sed "s/latest/$1/g"`
+template=`cat ${DEPLOYMENT_PATH} | sed "s/latest/v$1/g"`
 echo "$template" | kubectl apply -f -
